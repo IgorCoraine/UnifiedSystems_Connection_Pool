634cfb53df2a8ab6c326100278c1490b94bfd9df
Igor Coraine <igor.coraine@gmail.com>
2023-10-02 08:39:57 -0300

    Global definition area

---
+++ Global definition area.js
@@ -0,0 +1,4 @@
+export let conn_pool = []; //connection pool
+export let pool_size = 6; //max number of connections in the pool
+export let conn_count = 0;
+export const connection_string = "FILEDSN=C://connection_string.dsn";
